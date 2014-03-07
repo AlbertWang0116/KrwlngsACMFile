ae1f3a035b92c4c8627f1846782a15c4bf02b93a
Albert <st.krwlng@gmail.com>
2014-03-07 23:30:00 +0800

    Add the standard code of highest-relabel.

--- AfterACM/Module/Maximum-Flow.cpp
+++ AfterACM/Module/Maximum-Flow.cpp
@@ -34,3 +34,58 @@ int dinic(int s, int t) {
 		f+=dinic_dfs(s, t, INF_FLOW);
 	}
 }
+
+
+
+//highest-relabel algorithm to get maximum flow
+//The algorithm is non-recursive
+//Using Hash or Map to merge multiple capacity edge.
+//Flow Parameter (Can be changed to long long):
+//	struct edge - cap, flow
+//	global array - cf
+//	hl_push - flow
+//Note of parameter
+//	N - the maximum number of vertices in graph
+//	highest_relabel :: mx - the smallest integer that bigger than the vertices index except source and sink.
+struct edge { int nxt, des, cap, flow, rev; };
+edge e[M];
+int way[N][N], hd[N], ce[N], dis[N], que[N], cf[N], blk[2*N], pst[N];
+void hl_push(int u, int v, int i, int flow) {
+	cf[u]-=flow; e[i].cap-=flow; e[i].flow+=flow;
+	cf[v]+=flow; e[e[i].rev].cap+=flow; e[e[i].rev].flow-=flow;
+}
+
+void hl_relabel(int u, int &v, int &lv, int mx) {
+	int i, j, k;
+	for (i=ce[u]=hd[u], dis[u]=2*N; i; i=e[i].nxt)
+		if (e[i].cap) dis[u]=getmin(dis[u], dis[e[i].des]+1);
+	if (~v) pst[v]=pst[u]; else blk[lv]=pst[u];
+	pst[u]=blk[dis[u]]; blk[dis[u]]=u;
+	if (!~blk[lv] && dis[u]<mx)
+		for (i=lv+1; i<mx; ++i) for (j=blk[i], k=pst[j]; ~j; blk[i]=j=k, k=~k?pst[k]:0) {
+			dis[j]=mx+1; pst[j]=blk[dis[j]]; blk[dis[j]]=j;
+		}
+	lv=dis[u]; v=-1;
+}
+
+int highest_relabel(int s, int t, int mx) {
+	int i, u, v, head, tail, lv;
+	memset(dis, -1, sizeof(dis)); dis[t]=0;
+	que[0]=t; head=tail=0;
+	while (head<=tail) {
+		u=que[head++];
+		for (i=hd[u]; i; i=e[i].nxt)
+			if (e[e[i].rev].cap && dis[e[i].des]<0) dis[que[++tail]=e[i].des] = dis[u]+1;
+	}
+	for (i=0; i<N; ++i) if (dis[i]<0) dis[i]=mx+3; dis[s]=mx+2;
+	memcpy(ce, hd, sizeof(hd)); memset(blk, -1, sizeof(blk));
+	for (i=0; i<mx; ++i) if (i!=s&&i!=t) { pst[i]=blk[dis[i]]; blk[dis[i]]=i; }
+	memset(cf, 0, sizeof(cf)); cf[s]=INF_FLOW;
+	for (i=hd[s]; i; i=e[i].nxt) hl_push(s, e[i].des, i, e[i].cap);
+	for (lv=mx+3, u=blk[lv], v=-1; lv; v=u, u=~u?pst[u]:blk[--lv]) while (~u&&cf[u]) {
+		for (i=ce[u]; cf[u]&&i; i=cf[u]||!e[i].cap?e[i].nxt:i)
+			if (e[i].cap && dis[e[i].des]==dis[u]-1) hl_push(u, e[i].des, i, getmin(e[i].cap, cf[u]));
+		if (cf[u]) hl_relabel(u, v, lv, mx+2); else ce[u]=i;
+	}
+	return cf[t];
+}
