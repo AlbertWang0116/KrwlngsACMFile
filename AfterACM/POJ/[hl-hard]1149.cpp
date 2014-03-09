e9772cb7dd3e80b46a5eb15387cc6d20ae15e279
Albert <st.krwlng@gmail.com>
2014-03-09 13:20:35 +0800

    POJ 1149 - PIGS Problem : M pig houses, each houses has p[i] pigs. N customers come in order, each customer open some pig houses and buy q[i] pigs, after that, pig can change house in opened houses, then pig houses close and wait the next customer come. Find a exchange way, to make the sold pigs maximum. Solution : Each customer is a vertice. If a customer is the first person to open some pig house, connect an edge from source to that customer, with capacity of p[pig]. Otherwise, connect an edge from previous customer who can open this pig house to current customer, with inf capacity. Each customer connect an edge from it to sink, with capacity q[customer]. Proof : A pig's any kind of exchange and sale (after exchange) can be represent by a unit flow, vice versa.

---
+++ AfterACM/POJ/[hl-hard]1149.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+#include<map>
+using namespace std;
+
+struct edge { int nxt, des, cap, flow, rev; };
+#define N 120
+#define M 51000
+#define L 1100
+int way[N][N], hd[N], ce[N], dis[N], que[N], cf[N], blk[2*N], pst[N];
+int x[L], y[L];
+edge e[M];
+int n, m, cnt;
+const int ss = 110;
+const int tt = 111;
+const int INF_FLOW = 1000010;
+
+inline int getmin(int x, int y) { return x<y?x:y; }
+
+void insert(int x, int y, int z) {
+	if (!way[x][y]) {
+		way[x][y]=++cnt;
+		if (way[y][x]) { e[cnt].rev=way[y][x]; e[way[y][x]].rev=cnt; }
+		e[cnt].nxt=hd[x]; e[cnt].des=y; e[cnt].cap=e[cnt].flow=0; hd[x]=cnt;
+	}
+	e[way[x][y]].cap+=z;
+}
+
+void hl_push(int u, int v, int i, int flow) {
+	cf[u]-=flow; e[i].cap-=flow; e[i].flow+=flow;
+	cf[v]+=flow; e[e[i].rev].cap+=flow; e[e[i].rev].flow-=flow;
+}
+
+int hl_relabel(int u, int &v, int &lv, int mx) {
+	int i, j, k;
+	for (i=ce[u]=hd[u], dis[u]=2*N; i; i=e[i].nxt)
+		if (e[i].cap) dis[u]=getmin(dis[u], dis[e[i].des]+1);
+	if (~v) pst[v]=pst[u]; else blk[lv]=pst[u];
+	pst[u]=blk[dis[u]]; blk[dis[u]]=u;
+	if (!~blk[lv] && dis[u]<mx)
+		for (i=lv+1; i<mx; ++i) for (j=blk[i], k=pst[j]; ~j; blk[i]=j=k, k=~k?pst[k]:0) {
+			dis[j]=mx+1; pst[j]=blk[dis[j]]; blk[dis[j]]=j;
+		}
+	lv=dis[u]; v=-1;
+}
+
+int highest_relabel(int s, int t, int mx) {
+	int i, u, v, head, tail, lv;
+	memset(dis, -1, sizeof(dis)); memcpy(ce, hd, sizeof(hd)); dis[t]=0;
+	que[0]=t; head=tail=0;
+	while (head<=tail) {
+		u=que[head++];
+		for (i=hd[u]; i; i=e[i].nxt)
+			if (e[e[i].rev].cap && dis[e[i].des]<0) dis[que[++tail]=e[i].des] = dis[u]+1;
+	}
+	for (i=0; i<N; ++i) if (dis[i]<0) dis[i]=mx+3; dis[s]=mx+2;
+	memset(blk, -1, sizeof(blk));
+	for (i=0; i<mx; ++i) if (i!=s&&i!=t) { pst[i]=blk[dis[i]]; blk[dis[i]]=i; }
+	memset(cf, 0, sizeof(cf)); cf[s]=INF_FLOW;
+	for (i=hd[s]; i; i=e[i].nxt) hl_push(s, e[i].des, i, e[i].cap);
+	for (lv=mx+3, u=blk[lv], v=-1; lv; v=u, u=~u?pst[u]:blk[--lv]) while (~u&&cf[u]) {
+		for (i=ce[u]; cf[u]&&i; i=cf[u]||!e[i].cap?e[i].nxt:i)
+			if (e[i].cap && dis[e[i].des]==dis[u]-1) hl_push(u, e[i].des, i, getmin(cf[u], e[i].cap));
+		if (cf[u]) hl_relabel(u, v, lv, mx+2); else ce[u]=i;
+	}
+	return cf[t];
+}
+
+void conduct() {
+	int i, tmp, tmp2;
+	scanf("%d%d", &m, &n);
+	for (i=0; i<m; ++i) { x[i]=-1; scanf("%d", &y[i]); }
+	memset(way, 0, sizeof(way)); memset(hd, 0, sizeof(hd)); cnt=0;
+	for (i=0; i<n; ++i) {
+		scanf("%d", &tmp);
+		while (tmp--) {
+			scanf("%d", &tmp2); --tmp2;
+			if (!~x[tmp2]) { insert(ss, i, y[tmp2]); insert(i, ss, 0); }
+			else { insert(x[tmp2], i, INF_FLOW); insert(i, x[tmp2], 0); }
+			x[tmp2]=i;
+		}
+		scanf("%d", &tmp);
+		insert(i, tt, tmp); insert(tt, i, 0);
+	}
+	printf("%d\n", highest_relabel(ss, tt, n));
+}
+
+int main() {
+	conduct();
+	return 0;
+}
