9d80b64e0593daab321aee42ea5138b91e42efac
Albert <st.krwlng@gmail.com>
2014-04-15 02:01:39 +0800

    Add the standard code of tarjan.

--- AfterACM/Module/Graph-Theory.cpp
+++ AfterACM/Module/Graph-Theory.cpp
@@ -130,3 +130,29 @@ int kruskal(int n, int m) {
 		if (j<m) { merge_root(e[seq[j]].src, e[seq[j]].des); use[seq[j]]=1; } else return -1;
 	} return 0;
 }
+
+//Tarjan to get the ancestor and highest ancestor in dfs tree of graph.
+//array h is the ancestor, hh is highest ancestor, d stores the id.
+//The vertices' id is 0~n-1. If it is different, modify the input and 2nd line for loop of tarjan().
+struct edge { int des, nxt; };
+edge e[M];
+int hd[N], vst[N], stk[N], d[N], h[N], hh[N];
+int top, id;
+
+inline void tarjan_up(int x, int y) { h[y]=!vst[x]&&d[x]<d[h[y]]?x:h[y]; }
+int tarjan_dfs(int x) {
+	int i, p, v;
+	vst[x]=0; d[x]=id++; stk[top++]=x; h[x]=x; p=d[x];
+	for (i=hd[x]; i; i=e[i].nxt)
+		if (!~vst[v=e[i].des]) { p=getmin(p, tarjan_dfs(v)); tarjan_up(h[v], x); }
+		else if (vst[v]==1) { p=getmin(p, d[v]); tarjan_up(h[v], x); }
+		else tarjan_up(v, x);
+	if (x!=h[x] || p!=d[x]) vst[x]=1;
+	else do { top--; vst[stk[top]]=2; hh[stk[top]]=x; } while (stk[top]!=x);
+	return p;
+}
+
+void tarjan(int n) {
+	memset(vst, -1, sizeof(vst)); top=id=0;
+	for (int i=0; i<n; ++i) if (!~vst[i]) tarjan_dfs(i);
+}
